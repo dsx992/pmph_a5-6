6f5970ea5eaeca298e1d6fb5333ebea78d1f2727
dsx992 <dsx992@hendrixfut03fl.unicph.domain>
2025-11-04 16:02:50 +0100

    store ændringer i make_input og format_input + cuda programmet bruger make_input.
    før ville ./make_input 100 lave et irregular array på størrelse 100 af floats. Det bliver gjort
    med ./make_input -n 100 -f. For at gøre det futhark readable vil det være ./make_input -n 100 -f | .
    /format_input -f, hvor "-f" er float. Cuda skal bruge -u (unsigned int). derudover er der -i -- norm
    al int.
    For regular arrays, som før var ./make_input --regular 100 10, er det nu ./make_input -n 100 -m
    10 -f. Og igen kan det bare pipes ind i ./format_input -f.
    
    For at teste på samme input med cuda og futhark vil det være:
    ./make_input -n 100 -m 10 -u > 1.in
    cat 1.in | ./format_input -u > 1f.in
    cat 1.in | ./cuda_program
    echo "-- compiled input @ 1f.in" > test.fut
    .. you know the rest

--- make_input.c
+++ make_input.c
@@ -2,58 +2,160 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <time.h>
 
-#define A_MAX 2024
-static char* regular_f = "--regular";
+enum Type {
+    NOT_ASSIGNED,
+    INT,
+    UINT,
+    FLOAT
+};
+
+int writeIntA(int n)
+{
+    size_t sz = 2024;
+    time_t seed = time(NULL);
+    int buf[sz];
+
+    for (int i = 0; i < n; i+=sz) {
+        #pragma omp parallel for schedule(static)
+        for (int j = 0; j < sz; j++) {
+            srand(seed + j);
+            buf[j] = rand() - (RAND_MAX / 2);
+        }
+        int rest = n - i;
+        int min = rest < sz ? rest : sz;
+        fwrite(buf, sizeof(int), min, stdout);
+    }
+    return 0;
+}
+
+int writeUintA(int n)
+{
+    size_t sz = 2024;
+    time_t seed = time(NULL);
+    unsigned int buf[sz];
+
+    for (int i = 0; i < n; i+=sz) {
+        #pragma omp parallel for schedule(static)
+        for (int j = 0; j < sz; j++) {
+            srand(seed + j);
+            buf[j] = rand() - (RAND_MAX / 2);
+        }
+        int rest = n - i;
+        int min = rest < sz ? rest : sz;
+        fwrite(buf, sizeof(int), min, stdout);
+    }
+    return 0;
+}
+
+int writeFloatA(int n)
+{
+    size_t sz = 2024;
+    time_t seed = time(NULL);
+    float buf[sz];
+
+    for (int i = 0; i < n; i+=sz) {
+        #pragma omp parallel for schedule(static)
+        for (int j = 0; j < sz; j++) {
+            srand(seed + j);
+            buf[j] = rand() - (RAND_MAX / 2);
+        }
+        int rest = n - i;
+        int min = rest < sz ? rest : sz;
+        fwrite(buf, sizeof(int), min, stdout);
+    }
+    return 0;
+}
+
+int mkShpmInTmp(int n, int* tmp, int *m) {
+    for (int _n = n; _n > 0; (*m)++) {
+        int v = (rand() % _n) + 1;
+        tmp[*m] = v;
+        _n -= v;
+    }
+    return 0;
+}
 
 int main(int argc, char** argv) 
 {
-    if (strcmp("--help", argv[1]) == 0) {
-        printf("./make_input [n size] giver et irregular array\n");
-        printf("./make_input --regular [n size] [m size] giver et regulært array\n");
-        return 2;
-    }
-    int n, m, r;
-    if (argc == 4 
-        && strcmp(regular_f, argv[1]) == 0) {
+    int n, m, t;
 
-        n = atoi(argv[2]);
-        r = atoi(argv[3]);
-        if (n % r) return 1;
-        m = n / r;
-    } else {
-        n = atoi(argv[1]);
-        m = 0;
-        r = 0;
+    n = 0, m = 0, t = NOT_ASSIGNED;
+    for (int opt; (opt = getopt(argc, argv, "m:n:uif")) != -1; ) {
+        switch(opt) {
+            case 'm':
+                m = atoi(optarg);
+                break;
+            case 'n':
+                n = atoi(optarg);
+                break;
+            case 'u':
+                t = UINT;
+                break;
+            case 'i':
+                t = INT;
+                break;
+            case 'f':
+                t = FLOAT;
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-n int] [-m int] [-u] [-i] [-f]\n", 
+                        argv[0]);
+                exit(5);
+        }
     }
 
-    float* A = malloc(sizeof(float) * n);
+    if (m && n % m) fprintf(stderr, "m=%i går ikke op i n=%i.\n", m, n);
+
     int* II1 = malloc(sizeof(int) * n);
+    int* shp;
+    int* ks;
 
-    for (int _n = n; !r && _n; m++) {
-        int v = (rand() % _n) + 1;
+    if (m) {
+        shp = malloc(sizeof(int) * m);
+        int r = n / m;
+        #pragma omp parallel for schedule(static)
+        for (int i = 0; i < m; i++) {
+            shp[i] = r;
+        }
+    } else {
         // Bruger II1 som en temp array lige nu,
         // dette er ikke dens rigtige værdier
-        II1[m] = v;
-        _n -= v;
+        mkShpmInTmp(n, II1, &m);
+        shp = malloc(sizeof(int) * m);
+ 
+        #pragma omp parallel for schedule(static)
+        for (int i = 0; i < m; i++) {
+            shp[i] = II1[i];
+        }
     }
+    ks = malloc(sizeof(int) * m);
 
-    int shp[m];
-    int ks[m];
+    printf("%i\n", n);
+    printf("%i\n", m);
+
+    switch(t) {
+        case NOT_ASSIGNED:
+            fprintf(stderr, "der skal angives en type; -i, -u, el. -f\n");
+            return(8);
+        case INT:
+            writeIntA(n);
+            break;
+        case UINT:
+            writeUintA(n);
+            break;
+        case FLOAT:
+            writeFloatA(n);
+            break;
+        default:
+            fprintf(stderr, "ikke en ægte type\n");
+            return(7);
+    }
 
     #pragma omp parallel for schedule(static)
     for (int i = 0; i < m; i++) {
-        int d = r ? r : II1[i];
-        shp[i] = d;
-        ks[i] = (rand() % d) + 1;
-    }
-
-    #pragma omp parallel for schedule(static)
-    for (int i = 0; i < n; i++) {
-        int r = rand() % (2024 << 2) + 1;
-        int a = ((float) r) / 2;
-        a = rand() % 2 ? a * -1 : a;
-        A[i] = ((float) r) / 2;
+        ks[i] = (rand() % shp[i]) + 1;
     }
 
     #pragma omp parallel for schedule(static)
@@ -63,14 +165,12 @@ int main(int argc, char** argv)
         II1[i] = j;
     }
 
-    printf("%i\n", n);
-    printf("%i\n", m);
-    fwrite(A, sizeof(float), n, stdout);
     fwrite(II1, sizeof(int), n, stdout);
     fwrite(shp, sizeof(int), m, stdout);
     fwrite(ks, sizeof(int), m, stdout);
 
-    free(A);
     free(II1);
+    free(shp);
+    free(ks);
     return 0;
 }
